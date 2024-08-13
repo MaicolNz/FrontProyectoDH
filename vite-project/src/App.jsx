3d849eb770f8cdf0b651000ddd0958b8e66f6112
Maicol Nuñez <110599461+MaicolNz@users.noreply.github.com>
2024-08-13 01:20:43 -0600

    Detail y DetailView

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,24 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../src/Routes/Home';
-import Detail from '../src/Routes/Detail';
-import Layout from '../src/Layout/Layout';
-import DetailView from '../src/Routes/DetailView';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Home from './Routes/Home';
+import Detail from './Routes/Detail/Detail';
+import DetailView from './Routes/DetailView/DetailView';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importa Bootstrap JS (incluye Popper.js)
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/detail/:id" element={<Detail />} />
-            <Route path="/DetailView/:id" element={<DetailView />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/detailview/:id" element={<DetailView />} />
         </Routes>
       </Layout>
-</Router>
+    </Router>
   );
 };
-
 export default App;
--- vite-project/src/Layout/Layout.jsx
+++ vite-project/src/Layout/Layout.jsx
@@ -1,16 +1,19 @@
+// Layout.js
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
 import HeroContent from '../Components/HeroContent';
+import Footer from '../Components/Footer';
 
 const Layout = ({ children }) => {
   const location = useLocation();
+  const showHeroContent = location.pathname === '/'; // Muestra HeroContent solo en la página de inicio
+
   return (
     <>
       <Navbar />
       <div>
-      <HeroContent />
+        {showHeroContent && <HeroContent />}
         {children}
       </div>
       <Footer />
