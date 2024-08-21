f0fb78f2fa3c165a0530e9186ee616ac65bcf13e
nicos91189 <nicolassanchez91189@gmail.com>
2024-08-21 16:31:37 -0300

    ajuste detail

--- vite-project/src/Routes/Detail/Detail.jsx
+++ vite-project/src/Routes/Detail/Detail.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import instruments from '../../Components/utils/instruments.json';
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Detail = () => {
     const { id } = useParams();
     const instrumento = instruments.find(item => item.id === parseInt(id));
     const navigate = useNavigate();
-    const [count, setCount] = useState(0);
 
     const handleClick = () => {
         navigate(`/`);
@@ -18,45 +16,42 @@ const Detail = () => {
         navigate(`/DetailView/${id}`);
     };
 
-    const handleIncrease = () => {
-        setCount(count + 1);
-    };
-
-    const handleDecrease = () => {
-        if (count > 0) {
-            setCount(count - 1);
-        }
-    };
-
     if (!instrumento) {
         return <div>Instrumento no encontrado</div>;
     }
 
     return (
-        <div className="hero-content detail-hero">
-            <div className="container bg-color py-5">
-                <div className="row">
+        <div className="hero-content-detail detail-hero">
+            <div className="container bg-color py-3">
+                <div className="d-flex justify-content-end">
+                    <img
+                        src="../public/images/icons/iconBack.svg"
+                        alt="Volver atrás"
+                        onClick={handleClick}
+                        style={{ cursor: 'pointer' }}
+                        className="me-2"
+                    />
+                    <p className="mb-0" style={{ cursor: 'pointer' }} onClick={handleClick}>
+                        Volver Atrás
+                    </p>
+                </div>
+                <h1 className='instrument-title d-flex justify-content-center'>
+                    {instrumento.instrumento}
+                </h1>
+                <div className="detail-block-content">
+                    <div className="row">
 
-
-                    <div className="col-md-6">
-                        <div className="d-flex align-items-center mb-3">
+                        <div className="col-12 col-md-6 d-flex justify-content-center img-border">
                             <img
-                                src="../public/images/icons/iconBack.svg"
-                                alt="Volver atrás"
-                                onClick={handleClick}
-                                style={{ cursor: 'pointer' }}
-                                className="me-2"
+                                src={instrumento.img}
+                                alt={instrumento.instrumento}
+                                className="img-thumbnail img-detail img-fluid"
                             />
-                            <p className="mb-0" style={{ cursor: 'pointer' }} onClick={handleClick}>
-                                Volver atrás
-                            </p>
                         </div>
-
-                        <div className="detail-block-content">
-                            <h1>{instrumento.instrumento}</h1>
-                            <p className="detail-detalleView">
+                        <div className="col-md-6">
+                            <div className='detail-detalleView'>
                                 {instrumento.detalleView}
-                            </p>
+                            </div>
                             <p className="detail-precio">
                                 ${instrumento.precioAlquiler}/día
                             </p>
@@ -73,14 +68,6 @@ const Detail = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 d-flex justify-content-center img-border">
-                        <img
-                            src={instrumento.img}
-                            alt={instrumento.instrumento}
-                            className="img-thumbnail img-detail img-fluid"
-                        />
-                    </div>
-                    
                 </div>
             </div>
         </div>
