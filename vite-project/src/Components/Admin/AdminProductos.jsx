c831ebf2593631feb00f3c807226db97011018c6
Maicol Nuñez <110599461+MaicolNz@users.noreply.github.com>
2024-09-20 20:00:15 -0300

    Initial commit 6

--- vite-project/src/Components/Admin/AdminProductos.jsx
+++ vite-project/src/Components/Admin/AdminProductos.jsx
@@ -60,7 +60,7 @@ const AdminProductos = () => {
 
     const handleDelete = async (id) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/admin/instrumento/eliminar/${id}`, {
+            const response = await fetch(`https://backendproyectodh-production.up.railway.app/api/admin/instrumento/eliminar/${id}`, {
                 method: 'DELETE',
                 headers: {
                     'Authorization': `Bearer ${token}`, // Enviar el token en el encabezado
