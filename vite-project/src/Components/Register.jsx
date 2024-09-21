043a25219ce09936f0d7d3dacf1151f86b36059a
Maicol Nuñez <110599461+MaicolNz@users.noreply.github.com>
2024-09-20 21:30:53 -0300

    Initial commit 6

--- vite-project/src/Components/Register.jsx
+++ vite-project/src/Components/Register.jsx
@@ -43,8 +43,7 @@ const Register = () => {
             nombre,
             apellido,
             email: email,
-            password: password,
-            role:'USUARIO'
+            password: password
           }),
         });
 
